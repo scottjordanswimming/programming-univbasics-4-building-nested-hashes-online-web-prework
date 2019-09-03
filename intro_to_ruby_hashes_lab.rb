22d80ad255719e7dbdc4ae48f856f60e5d69b876
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-03 20:13:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,9 +31,26 @@ monopoly =  {
       :four_pieces_owned=>200
     },
     :names=>
-    {
+   {
+      :reading_railroad=>
+      {
+        
+      },
+      :pennsylvania_railroad=>
+      {
+        
+      },
+      :b_and_o_railroad=>
+      {
+        
+      },
+      :shortline=>
+      {
+        
+      }
     }
-  }}
+  }
+}
 end
 
 def monopoly_with_fourth_tier
